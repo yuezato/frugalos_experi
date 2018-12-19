86227c7f2d7eed227601be6c760604b0df3fe8c4
yuuya uezato <yuuya_uezato@dwango.co.jp>
2018-12-19 23:27:22 +0900

    CannyLsのデバイスが予想以上に遅い件の調査

---
+++ src/main.rs
@@ -0,0 +1,81 @@
+extern crate mpsc_use;
+use mpsc_use::queue::{Deadline, DeadlineQueue};
+
+use fibers::sync::oneshot;
+use futures::{Future, Poll};
+use std::sync::mpsc::{Receiver, Sender};
+use std::thread;
+
+struct Command {
+    value: u128,
+    chan: AsyncReply<u128>,
+}
+ 
+#[derive(Debug)]
+pub struct AsyncResult<T>(oneshot::Monitor<T, ()>);
+impl<T> AsyncResult<T> {
+    #[allow(clippy::new_ret_no_self)]
+    fn new() -> (AsyncReply<T>, Self) {
+        let (tx, rx) = oneshot::monitor();
+        (AsyncReply(tx), AsyncResult(rx))
+    }
+}
+impl<T> Future for AsyncResult<T> {
+    type Item = T;
+    type Error = String;
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        self
+            .0
+            .poll()
+            .map_err(|e| format!("{:?}", e))
+    }
+}
+
+#[derive(Debug)]
+struct AsyncReply<T>(oneshot::Monitored<T, ()>);
+impl<T> AsyncReply<T> {
+    fn send(self, result: Result<T, ()>) {
+        self.0.exit(result);
+    }
+}
+
+fn wait<E, F: Future<Error = E>>(mut f: F) -> Result<(), E> {
+    while !(f.poll())?.is_ready() {}
+    Ok(())
+}
+
+type MySender = Sender<Command>;
+type MyReceiver = Receiver<Command>;
+
+fn main() {
+    const NUM: u128 = 2587_8531;
+
+    let mut queue = DeadlineQueue::new();
+    let (command_tx, command_rx): (MySender, MyReceiver) = std::sync::mpsc::channel();
+
+    thread::spawn(move || {
+        let mut recv = 0;
+        loop {
+            if let Ok(com) = command_rx.try_recv() {
+                queue.push(com, Deadline::Infinity);
+                recv += 1;
+            } else if let Some(com) = queue.pop() {
+                com.chan.send(Ok(com.value));
+            }
+            if recv == NUM {
+                break;
+            }
+        }
+    });
+
+    println!("start");
+    
+    for i in 0..NUM {
+        let (reply, result) = AsyncResult::new();
+        command_tx.send( Command { value: i, chan: reply } ).unwrap();
+        wait(result).unwrap();
+        
+    }
+    
+    println!("end");
+}
---
+++ src/queue.rs
@@ -0,0 +1,95 @@
+use std::cmp;
+use std::collections::BinaryHeap;
+use std::time::{Instant, Duration};
+
+#[derive(Debug, Clone, Copy, PartialOrd, Ord, PartialEq, Eq, Hash)]
+pub enum Deadline {
+    Immediate,
+    Within(Duration),
+    Infinity,
+}
+impl Default for Deadline {
+    fn default() -> Self {
+        Deadline::Infinity
+    }
+}
+
+#[derive(Debug)]
+pub struct DeadlineQueue<T> {
+    seqno: u64,
+    heap: BinaryHeap<Item<T>>,
+}
+impl<T> DeadlineQueue<T> {
+    /// 新しい`DeadlineQueue`インスタンスを生成する.
+    pub fn new() -> Self {
+        DeadlineQueue {
+            seqno: 0,
+            heap: BinaryHeap::new(),
+        }
+    }
+
+    /// 新しいコマンドをキューに追加する.
+    pub fn push(&mut self, v: T, deadline: Deadline) {
+        let deadline = AbsoluteDeadline::new(deadline);
+        let item = Item {
+            seqno: self.seqno,
+            item: v,
+            deadline,
+        };
+        self.heap.push(item);
+        self.seqno += 1;
+    }
+
+    /// 次に処理するコマンドを取り出す.
+    pub fn pop(&mut self) -> Option<T> {
+        self.heap.pop().map(|t| t.item)
+    }
+
+    /// キューに格納されている要素数を返す.
+    pub fn len(&self) -> usize {
+        self.heap.len()
+    }
+}
+
+/// ヒープに格納する要素.
+#[derive(Debug)]
+struct Item<T> {
+    seqno: u64, // デッドラインが同じ要素をFIFO順で扱うためのシーケンス番号
+    item: T,
+    deadline: AbsoluteDeadline,
+}
+impl<T> PartialEq for Item<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.seqno == other.seqno
+    }
+}
+impl<T> Eq for Item<T> {}
+impl<T> PartialOrd for Item<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl<T> Ord for Item<T> {
+    fn cmp(&self, other: &Self) -> cmp::Ordering {
+        other
+            .deadline
+            .cmp(&self.deadline)
+            .then_with(|| other.seqno.cmp(&self.seqno))
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum AbsoluteDeadline {
+    Immediate,
+    Until(Instant),
+    Infinity,
+}
+impl AbsoluteDeadline {
+    fn new(relative: Deadline) -> Self {
+        match relative {
+            Deadline::Immediate => AbsoluteDeadline::Immediate,
+            Deadline::Within(d) => AbsoluteDeadline::Until(Instant::now() + d),
+            Deadline::Infinity => AbsoluteDeadline::Infinity,
+        }
+    }
+}
